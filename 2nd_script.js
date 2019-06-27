ae0c91a04edd35d706ca6c6ccd4f0f3ba69282be
Hannah Aurand <aurand.hannah@gmail.com>
2019-06-27 15:37:20 -0400

    Creating the fiver count down

--- 2nd_script.js
+++ 2nd_script.js
@@ -4,27 +4,29 @@ var app = new Vue({
     el: '#time_left',
     data: {
         //Ideally this event_date would be grabbed from the server-side, but for now it is hard-coded.
-        event_date: new Date()
+        current_date: new Date()
     },
     methods: {
 
-        calculate_days_left: function (event_date) {
+        calculate_days_left: function (current_date) {
+            //First I must grab the current date and time 
+            let today = this.current_date;
+            today.setHours(24, 0, 0);
+            console.log(today);
+            webinar_date = today.getDate() + 5;
+            console.log(webinar_date);
+            webinar_ms = Math.floor(Date.parse(webinar_date));
+            today_ms = Math.floor(Date.parse(today));
+            console.log(webinar_ms);
+            console.log(today_ms);
+            five_day_count = Math.floor(webinar_ms - today_ms);
+            console.log(five_day_count);
 
-            let time = Date.parse(this.event_date)
 
-            let days_left = Math.floor(time / (1000 * 60 * 60 * 24));
-            let hours_left = Math.floor(time / (1000 * 60 * 60));
-            let minutes_left = Math.floor((time / 1000 / 60) % 60);
-            let seconds_left = Math.floor((time / 1000) % 60);
 
-            return {
-                'time': time,
-                'days_left': days_left,
-                'hours_left': hours_left,
-                'minutes_left': minutes_left,
-                'seconds_left': seconds_left
-            }
+
         }
     }
 })
 
+let time = Date.parse(this.event_date) - Date.parse(new Date());

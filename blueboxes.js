7e622dda56327937af7c7a4e7987c6bb45260787
Hannah Aurand <aurand.hannah@gmail.com>
2019-06-28 15:34:16 -0400

    button-click functionality added. Used an alert box instead of a new element--strange problem with document.textContent not writing to the element below the button

--- blueboxes.js
+++ blueboxes.js
@@ -1,6 +1,53 @@
+
+
 // 2. Make it so when someone clicks either of the two blue buttons below the checkboxes and the checkboxes are not checked an alert appears asking the user to check the checkbox before proceeding.
 
-// onclick, check that the checkboxes are set to true. If notDeepEqual, alert the user to do check them
+//I realize I need to make these reusable so that I can put them all into one function and save space. Currently going for "done," not "perfect."
+
+const button1 = document.getElementById('button1');
+button1.addEventListener('click', function () {
+    let isChecked = document.getElementById('checkbox1').checked;
+    if (!isChecked) {
+        alert("Please check one of the Checkmarks for the newsletter to continue.")
+    } else if (isChecked) {
+        apiCall();
+    }
+});
+
+const button2 = document.getElementById('button2');
+button2.addEventListener('click', function () {
+    let isChecked = document.getElementById('checkbox2').checked;
+
+    if (!isChecked) {
+        alert("Please check one of the Checkmarks for the newsletter to continue.")
+    } else {
+        apiCall();
+    }
+});
+
+
+let apiCall = function () {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let obj = this.responseText;
+            const text = "Congratulations! Your spot is reserved"
+            const regex = /Congratulations! Your spot is reserved/;
+            const found = text.match(regex);
+            console.log(found[0]);
+            let success = document.getElementsByClassName('success');
+            console.log(success);
+            success.textContent = found[0];
+            alert(found[0]);
+      
+
+        }
+    };
+    xhttp.open("GET", "https://bl45immth4.execute-api.us-east-1.amazonaws.com/production/", true);
+    xhttp.send();
+
+
+}
 
 // When the user clicks either of the two blue buttons below the checkboxes and the checkboxes are checked make a call to the following endpoint: 
 // https://bl45immth4.execute-api.us-east-1.amazonaws.com/production/

4099d316a9d0904526eacf7dab120e51302b77c3
Hannah Aurand <aurand.hannah@gmail.com>
2019-06-28 12:03:50 -0400

    checkbox functionality added

--- checkbox.js
+++ checkbox.js
@@ -5,20 +5,66 @@
 //Need a checked: y/n boolean that is available to the blue button for the alert to request the user to check the box before they can proceed.
 //Both checkboxes need to have the same class. Grab them by the class, access their value
 
-// Checkbox: <input type="checkbox" id="myCheck">
 
-// <p>Click the "Try it" button to find out whether the checkbox is checked, or not.</p>
-
-// <button onclick="myFunction()">Try it</button>
-
-// <p id="demo"></p>
-
-// <script>
 // function myFunction() {
-//   var x = document.getElementById("myCheck").checked;
-//   document.getElementById("demo").innerHTML = x;
+
+//     var inputs, index;
+
+//     inputs = document.getElementsByTagName('input');
+//     // for (index = 0; index < inputs.length; ++index) {
+//     // deal with inputs[index] element.
+//     inputs.forEach({
+//         if(checked) {
+//             return
+//         }
+//     })
+
+//     console.log(checkmark.checked);
+// };
+
+//IT IS BEST TO MAKE THESE FUNCTIONS TO BE REUSABLE, BUT IN THE INTEREST OF TIME, I'M MOVING ON. 
+//DONE IS BETTER THAN PERFECT.
+var checkbox1 = document.getElementById('checkbox1');
+checkbox1.addEventListener('change', function CheckForm() {
+    // console.log('listener called');
+    var isChecked = checkbox1.checked;
+    if (isChecked) {
+        let checkbox2 = document.getElementById('checkbox2').checked = true;
+        // alert('checked');
+    } else { //unchecked
+        //execute code here
+        let checkbox2 = document.getElementById('checkbox2').checked = false;
+        console.log('not checked');
+    }
+})
+
+var checkbox2 = document.getElementById('checkbox2');
+checkbox2.addEventListener('change', function CheckForm() {
+    var isChecked = checkbox2.checked;
+    if (isChecked) {
+        let checkbox1 = document.getElementById('checkbox1').checked = true;
+    } else { //unchecked
+        //execute code here
+        let checkbox1 = document.getElementById('checkbox1').checked = false;
+        console.log('not checked');
+    }
+})
+
+
+
+
 // }
-// </script>
+
+// checkbox.addEventListener("change", functionname, false);
+// const checkboxes = [document.body.querySelector(input[type = 'checkbox'])];
+
+// console.log(checkboxes[0, 1]);
+// return checkboxes;
+// }
+
+
+
+
 
 //if either checkbox's state is true, set the other to true as well. 
 
